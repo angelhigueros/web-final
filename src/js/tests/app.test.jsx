86dc13d40024aa7733fd2a12e4251fbecd0ad6d8
Angel Higueros <angel.higueros01@gmail.com>
2022-06-02 16:32:12 -0600

    feat: proyecto final

--- src/js/tests/app.test.jsx
+++ src/js/tests/app.test.jsx
@@ -7,6 +7,10 @@ import 'jsdom-global/register';
 
 import AppScreen from '../components/appScreen';
 import Menu from '../components/menu';
+import About from '../components/about';
+import Work from '../components/work';
+import Skills from '../components/skills';
+import Contact from '../components/Contact';
 
 describe('Test en <AppScren /> ', () => {
   test('Se debe mostrar el componente correctamente', () => {
@@ -17,10 +21,72 @@ describe('Test en <AppScren /> ', () => {
   test('Los textos se muestran correctamente en Menu', () => {
     const wrapper = mount(<Menu />);
 
-    const title = wrapper.find('h4').text();
-    expect(title).toBe('Angel Higueros');
+    const title = wrapper.find('span').text();
+    expect(title).toBe('Full Stack React developer');
 
     const screen = wrapper.find('h4').text();
     expect(screen).toBe('Angel Higueros');
+  });
+
+  test('Se debe mostrar el componente correctamente', () => {
+    const pantalla = mount(<Menu />);
+    expect(pantalla).toMatchSnapshot();
+  });
+
+  test('Los textos se muestran correctamente en About', () => {
+    const wrapper = mount(<About />);
+
+    const title = wrapper.find('h1').text();
+    expect(title).toBe('SOBRE MI');
+
+    const screen = wrapper.find('h3').text();
+    expect(screen).toBe('Quien soy');
+  });
+
+  test('Se debe mostrar el componente correctamente', () => {
+    const pantalla = mount(<About />);
+    expect(pantalla).toMatchSnapshot();
+  });
+
+  test('Los textos se muestran correctamente en Skills', () => {
+    const wrapper = mount(<Skills />);
+
+    const title = wrapper.find('h1').text();
+    expect(title).toBe('MIS HABILIDADES');
+
+    const screen = wrapper.find('h3');
+    expect(screen[0]).toBe('Experiencia');
+    expect(screen[1]).toBe('Habilidades');
+    expect(screen[2]).toBe('Educación');
+    expect(screen[3]).toBe('Idiomas');
+  });
+
+  test('Se debe mostrar el componente correctamente', () => {
+    const pantalla = mount(<Skills />);
+    expect(pantalla).toMatchSnapshot();
+  });
+
+  test('Los textos se muestran correctamente en Contact', () => {
+    const wrapper = mount(<Contact />);
+
+    const title = wrapper.find('h1').text();
+    expect(title).toBe('HABLAME');
+  });
+
+  test('Se debe mostrar el componente correctamente', () => {
+    const pantalla = mount(<Contact />);
+    expect(pantalla).toMatchSnapshot();
+  });
+
+  test('Los textos se muestran correctamente en Work', () => {
+    const wrapper = mount(<Work />);
+
+    const title = wrapper.find('h1').text();
+    expect(title).toBe('MI PORTAFOLIO');
+  });
+
+  test('Se debe mostrar el componente correctamente', () => {
+    const pantalla = mount(<Work />);
+    expect(pantalla).toMatchSnapshot();
   });
 });
